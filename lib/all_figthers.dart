a80e984140138b6778e6ac6cc273b84d4803da3c
ihmaisrael@gmail.com <ihmaisrael@gmail.com>
2020-08-07 13:25:37 -0500

    Se separo la biografia del widget MyApp para que el context funcione correctamente dentro del navigator

---
+++ lib/all_figthers.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AllFighters extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mas Luchadores', style: TextStyle(fontSize: 30),
+        ),
+         backgroundColor: Colors.purpleAccent[700] ,
+      ),
+
+
+
+    );
+  }
+}
